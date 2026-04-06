86fb4ead2735653955f91ff6b9df51bc326b5dba
MrSinaf <mr@sinaf.me>
2026-04-06 23:57:34 +0200

    ヽ（＾□＾）ノ Moved Warhammer content to Gallery and revamped structure
    > Redirection de `/warhammer` vers `/warhammer/gallery` en modifiant `MainLayout`
    > Création d'une galerie pour afficher les personnages peints avec chargement dynamique
    > Désactivation temporaire des pages `Home` et `Faction` pour simplifier la navigation

---
+++ Components/Pages/Warhammer/Gallery.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sinaf.Me.Data;
+
+namespace Sinaf.Me.Components.Pages.Warhammer;
+
+public partial class Gallery
+{
+	private const int PAGE_SIZE = 10;
+	
+	private uint[] characters = [];
+	private int skip;
+	private bool hasMore = true;
+	private bool loading;
+	
+	protected override async Task OnInitializedAsync() => await LoadMoreAsync();
+	
+	private async Task LoadMoreAsync()
+	{
+		if (loading || !hasMore)
+			return;
+		
+		loading = true;
+		
+		await using var context = new WarhammerDbContext();
+		var data = await context.Characters
+								.OrderByDescending(x => x.Id)
+								.Skip(skip)
+								.Take(PAGE_SIZE)
+								.Select(x => x.Id)
+								.ToArrayAsync();
+		
+		characters = [..characters, ..data];
+		skip += data.Length;
+		hasMore = data.Length == PAGE_SIZE;
+		
+		loading = false;
+	}
+}
