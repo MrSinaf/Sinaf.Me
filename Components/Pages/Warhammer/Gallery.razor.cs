2cea45de87822b28f7dcf9274b6af848ae8d0b31
MrSinaf <mrsinaf@sinafproduction.xyz>
2026-06-08 16:05:56 +0200

    Enhanced Warhammer Gallery ( •̀ ω •́ )✧ with dynamic grid and better image handling
    > Mise à jour de la galerie pour utiliser une grille dynamique CSS.
    > Ajout du redimensionnement dynamique des vignettes via un script `setSize`.
    > Modification du modèle "Character" pour inclure les URLs des images et miniatures.
    > Amélioration du style CSS et meilleure organisation du code.

--- Components/Pages/Warhammer/Gallery.razor.cs
+++ Components/Pages/Warhammer/Gallery.razor.cs
@@ -1,18 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Sinaf.Me.Data;
+using Sinaf.Me.Data.Warhammer;
 
 namespace Sinaf.Me.Components.Pages.Warhammer;
 
 public partial class Gallery
 {
-	private uint[] characters = [];
+	private Character[] characters = [];
 	
 	protected override async Task OnInitializedAsync()
 	{
 		await using var context = new WarhammerDbContext();
 		characters = await context.Characters
 								  .OrderByDescending(x => x.Id)
-								  .Select(x => x.Id)
+								  .Select(x => new Character
+								  {
+									  Id = x.Id,
+									  Name = x.Name,
+									  UpdatedAt = x.UpdatedAt
+								  })
 								  .ToArrayAsync();
 	}
 }
